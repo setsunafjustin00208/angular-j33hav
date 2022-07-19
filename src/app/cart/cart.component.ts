e372b30901b7ecb71aee3b83e7b6b077ed51ccbd
setsunafjustin00208 <setsunafjustin00208@outlook.com>
2022-07-19 14:53:29 +0800

    2 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -20,6 +20,10 @@ export class CartComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+   
+  }
+  onSubmit(): void {
+    // Process checkout data here
     this.items = this.cartService.clearCart();
     console.warn('Your order has been submitted', this.checkoutForm.value);
     this.checkoutForm.reset();
